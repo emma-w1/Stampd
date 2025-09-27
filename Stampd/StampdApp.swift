7f6677d09b6fdb4fdfd2ee9882ab8c1aadef9cc7
Adishree Das <dasadishree@gmail.com>
2025-09-27 15:41:51 -0400

    Initial commit of Xcode project

---
+++ Stampd/StampdApp.swift
@@ -0,0 +1,17 @@
+//
+//  StampdApp.swift
+//  Stampd
+//
+//  Created by Adishree Das on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct StampdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
